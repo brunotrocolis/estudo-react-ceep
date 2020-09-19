ebcee283666d73b22ada9ee364dd109fef995aee
Bruno Trócolis <bruno.trocolis@gmail.com>
2020-09-19 10:18:21 -0300

    Base do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Escreva sua nota..."></textarea>
+        <button>Criar nota</button>
+      </form>
+      <ul>
+        <li>
+
+        </li>
+      </ul>
     </div>
   );
 }
